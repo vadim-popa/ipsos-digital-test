9a230d8d5dad7e2a3708707c62677f01d880a8e0
Vadim Popa <vadim_popa@yahoo.com>
2022-05-21 12:59:08 +0300

    store config

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
-// import example from './module-example'
 import listModule from './listModule';
 
 Vue.use(Vuex);
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-export default function (/* { ssrContext } */) {
+export default function () {
   const Store = new Vuex.Store({
     modules: {
       listModule,
     },
-
-    // enable strict mode (adds overhead!)
-    // for dev mode only
     strict: process.env.DEBUGGING,
   });
 
